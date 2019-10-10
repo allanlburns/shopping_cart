9953d68260c5d22228932ad7edd7d78dfcb6ad7e
Allan Burns <allanlburns@yahoo.com>
2019-10-10 16:40:55 -0400

    Refactored shopping cart app with object oriented.

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,35 +1,66 @@
-def shopping_cart():
+from IPython.display import clear_output
 
-        cart = []
+# have a class called cart that retains items and has methods to add, remove, and show
 
-        while True:
-            command = input("What would you like to do? (add item / delete item / view list / quit) ").lower()
+# step #1: create the cart class
+class Cart():
+    # when instantiated, cart object should define empty list for items
+    def __init__(self):
+        self.items = []
 
-            if command[0].lower() == 'q':
-                print("Quitting Cart Program")
-                break
+    # create a method to show cart
+    def showCart(self):
+        clear_output()
+        print("Items in your cart: ")
+        for item in cart.items:
+            print(item)
 
-            elif command[0] == 'a':
-                item = input("What item would you like to add? ")
-                cart.append(item)
-                print("Added Item")
-            elif command[0] == 'd':
-                item = input("What item would you like to delete? ")
-                try:
-                    cart.remove(item)
-                    print(f"Deleted {item}")
-                except:
-                    print(f"Item '{item}' not found in cart. Please try again. ")
+    # create a method to add to cart
+    def addItem(self, item):
+        clear_output()
+        self.items.append(item)
 
-            elif command[0] == 'v':
-                print("Items in Cart: ", end=' ')
-                for i in cart:
-                    print(i, end=' ')
-            else:
-                print("Please enter valid command.")
+    # create a method to remove from cart
+    def removeItem(self, item):
+        pass
 
-        # print cart when exiting program
-        for i in cart:
-            print("Items in cart: ", i, end=' ')
+    def clearCart(self):
+        clear_output()
+        print('Your cart is now empty.')
+        self.items.clear()
 
-shopping_cart()
+
+# create instance of cart object with empty list
+
+cart = Cart()
+
+
+# start the while loop until user quits
+
+shopping = True
+
+while shopping:
+    # ask for input
+    ans = input("Would you like to add/remove/show/quit? ").lower()
+
+    # base case
+    if ans == 'quit':
+        shopping = False
+    # ask if they would like to add, remove, show, perform steps using cart methods
+
+    elif ans == 'add':
+        item = input("What would you like to add? ")
+        cart.addItem(item)
+    elif ans == 'remove':
+        if len(cart) <= 0:
+            print("There are no items in the list")
+            continue
+        item = input("What would you like to remove? ")
+        cart.removeItem(item)
+    elif ans == 'show':
+        cart.showCart()
+    elif ans == 'clear':
+        cart.clearCart()
+
+    else:
+        print("That is not an option. Try again")
